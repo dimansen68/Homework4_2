0c1f865d166f79894be06ba2417b57747b47d2c5
dimansen68 <dimansen15@inbox.ru>
2020-12-29 11:24:59 +0300

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,16 +8,16 @@ public class Main {
       public void toLeft() {}
       2.Пройти с 1-го до предпоследнего элемента
      */
-        int[][] mas = new int[3][3];
+        int[][] mas = new int[10][10];
         int i, j, k = 0;
-        for (i = 0; i < 3; i++)
-            for (j = 0; j < 3; j++) {
+        for (i = 0; i < 10; i++)
+            for (j = 0; j < 10; j++) {
                 mas[i][j] = k;
                 k++;
             }
         ;
-        for (i = 0; i < 3; i++) {
-            for (j = 0; j < 3; j++)
+        for (i = 0; i < 10; i++) {
+            for (j = 0; j < 10; j++)
                 System.out.print(mas[i][j] + " ");
 
             ;
