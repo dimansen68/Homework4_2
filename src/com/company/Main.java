b208299837cd1a5b97f19848647f512241918cb4
dimansen68 <dimansen15@inbox.ru>
2021-01-17 14:29:55 +0300

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
       public void toLeft() {}
       2.Пройти с 1-го до предпоследнего элемента
      */
-        int[][] mas = {{1,2,3},{1},{1,2,3,4,5},{2,4,6,7}};
+        int[][] mas = {{1, 2, 3}, {1}, {1, 2, 3, 4, 5}, {2, 4, 6, 7}};
         for (int i = 0; i < mas.length; i++) {
             for (int j = 0; j < mas[i].length; j++)
                 System.out.print(mas[i][j] + " ");
